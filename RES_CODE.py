ed23347b975c94f430b722e2bb58e26e6d2a6b2a
uitae <65074073+uitae@users.noreply.github.com>
2020-08-02 19:13:59 +0900

    Update RES_CODE.py

--- RES_CODE.py
+++ RES_CODE.py
@@ -1,6 +1,12 @@
-#!/bin/bash
+/*#!/bin/bash
 RES_CODE=$(curl -o /dev/null -w "%{http_code}" "http://115.145.212.130:8080")
 
 if [ $STATUS -eq 200 ]; then
   echo "OKOK"
 fi
+*/
+
+
+import os
+os.system("curl -o /dev/null -w "%{http_code}" "http://115.145.212.130:8080"")
+
